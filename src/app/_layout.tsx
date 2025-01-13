142baa4930591594c8bb98797c929864d12f8882
BrunoAlan <alan.brunoab@gmail.com>
2025-01-13 14:12:44 -0300

    Refactor RootLayout to use LanguageProvider for localization support

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,29 +1,13 @@
 import { Stack } from 'expo-router';
 
-import { getLocales } from 'expo-localization';
-import i18next from 'i18next';
-
-import en from '../../static/langs/en.json';
-import es from '../../static/langs/es.json';
-
-const translations = {
-    en: { translation: { ...en } },
-    es: { translation: { ...es } },
-};
-
-export const i18nextInstance = i18next.init({
-    compatibilityJSON: 'v3',
-    ns: undefined,
-    fallbackLng: 'en',
-    lng: 'en',
-    resources: translations,
-});
-
+import LanguageProvider from '../providers/Language.provider';
 
 export default function RootLayout() {
     return (
-        <Stack>
-            <Stack.Screen name='index' />
-        </Stack>
+        <LanguageProvider>
+            <Stack screenOptions={{ headerShown: false }}>
+                <Stack.Screen name='index' />
+            </Stack>
+        </LanguageProvider>
     );
 }
