6d2d9d1a478d7bf57ae8a4990c52e831c271c4e3
BrunoAlan <alan.brunoab@gmail.com>
2025-01-13 14:12:23 -0300

    Enhance LogedToken component with language toggle and custom text rendering

--- src/app/(protected)/[logedToken].tsx
+++ src/app/(protected)/[logedToken].tsx
@@ -1,14 +1,32 @@
 import { useLocalSearchParams } from 'expo-router';
-import { Text, View } from 'react-native';
+import { Text, Pressable, View } from 'react-native';
+import { useLanguage } from '../../providers/Language.provider';
 import i18next from 'i18next';
+import { CustomText } from '@/src/components/CustomText';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+
 const LogedToken = () => {
     const { logedToken: token } = useLocalSearchParams();
+    const { currentLanguage, changeLanguage } = useLanguage();
+    const { top } = useSafeAreaInsets();
+
     return (
-        <View>
-            <Text style={{ textAlign: 'auto' }}>{token}</Text>
-            <Text>{i18next.t('bf.contactUs')}</Text>
+        <View style={{ flex: 1, marginTop: top }}>
+            <CustomText>{token?.toString()}</CustomText>
+            {/* Mostrar el texto traducido */}
+            <CustomText>{i18next.t('bf.contactUs')}</CustomText>
+
+            {/* Botón para cambiar el idioma */}
+            <Pressable
+                onPress={() => {
+                    const newLanguage = currentLanguage === 'en' ? 'ar' : 'en'; // Alternar entre inglés y árabe
+                    changeLanguage(newLanguage);
+                }}
+            >
+                <CustomText>Cambiar idioma</CustomText>
+            </Pressable>
         </View>
     );
 };
+
 export default LogedToken;
-// i18nLabel[bf.contactUs]
