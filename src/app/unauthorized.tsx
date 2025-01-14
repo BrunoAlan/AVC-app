4d99415a95a2ded1388b64a1176e09d5d855b90e
BrunoAlan <alan.brunoab@gmail.com>
2025-01-14 11:35:27 -0300

    Add unauthorized screen and navigation handling

---
+++ src/app/unauthorized.tsx
@@ -0,0 +1,11 @@
+import { View } from 'react-native';
+import { CustomText } from '../components/CustomText';
+
+function Unauthorized() {
+    return (
+        <View>
+            <CustomText>unauthorized</CustomText>
+        </View>
+    );
+}
+export default Unauthorized;
