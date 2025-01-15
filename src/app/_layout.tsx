073619bd57e83d17b9fdb93900a38f651df8016d
BrunoAlan <alan.brunoab@gmail.com>
2025-01-15 10:14:10 -0300

    Wrap RootLayout in QueryClientProvider for React Query integration

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,14 +1,19 @@
 import { Stack } from 'expo-router';
 
 import LanguageProvider from '../providers/Language.provider';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 export default function RootLayout() {
+    const queryClient = new QueryClient();
+
     return (
-        <LanguageProvider>
-            <Stack screenOptions={{ headerShown: false }}>
-                <Stack.Screen name='index' />
-                <Stack.Screen name='unauthorized' />
-            </Stack>
-        </LanguageProvider>
+        <QueryClientProvider client={queryClient}>
+            <LanguageProvider>
+                <Stack screenOptions={{ headerShown: false }}>
+                    <Stack.Screen name='index' />
+                    <Stack.Screen name='unauthorized' />
+                </Stack>
+            </LanguageProvider>
+        </QueryClientProvider>
     );
 }
