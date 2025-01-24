3825bfb793405f534038ec557b7933001ad521c5
BrunoAlan <alan.brunoab@gmail.com>
2025-01-24 10:46:38 -0300

    Implement custom drawer content with logout functionality and integrate button and text components

--- src/app/(drawer)/_layout.tsx
+++ src/app/(drawer)/_layout.tsx
@@ -1,10 +1,47 @@
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { Drawer } from 'expo-router/drawer';
+import { Button } from '@/components/ui/button';
+import { Text } from '@/components/ui/text';
+import {
+    DrawerContentScrollView,
+    DrawerItemList,
+} from '@react-navigation/drawer';
+import { StorageAdapter } from '@/src/config/adapters/storage-adapter';
+import { useRouter } from 'expo-router';
 
 export default function Layout() {
+    const router = useRouter();
     return (
         <GestureHandlerRootView style={{ flex: 1 }}>
-            <Drawer screenOptions={{ headerShown: false }} />
+            <Drawer
+                screenOptions={{ headerShown: false }}
+                drawerContent={(props) => (
+                    <DrawerContentScrollView
+                        className='flex flex-1 bg-red-300 px-4'
+                        {...props}
+                    >
+                        <DrawerItemList {...props} />
+                        <Button
+                            className='self-end'
+                            variant={'outline'}
+                            onPress={async () => {
+                                await StorageAdapter.removeItem('token');
+                                router.replace('/');
+                            }}
+                        >
+                            <Text>Logout</Text>
+                        </Button>
+                    </DrawerContentScrollView>
+                )}
+            >
+                <Drawer.Screen
+                    name='(tabs)'
+                    options={{
+                        drawerLabel: 'Tabs',
+                        drawerItemStyle: { display: 'none' },
+                    }}
+                />
+            </Drawer>
         </GestureHandlerRootView>
     );
 }
