08e1d564ed8ff991c1f45f7a9b7c43b4cc421eb9
BrunoAlan <alan.brunoab@gmail.com>
2025-01-25 18:28:54 -0300

    Hide drawer item in layout for improved navigation

--- src/app/(drawer)/_layout.tsx
+++ src/app/(drawer)/_layout.tsx
@@ -20,6 +20,9 @@ export default function Layout() {
                     name='(tabs)'
                     options={{
                         drawerLabel: 'Tabs',
+                        drawerItemStyle: {
+                            display: 'none',
+                        },
                     }}
                 />
             </Drawer>
