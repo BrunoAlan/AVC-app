7f6f124eee5257a1a2e83faab4fe9f9b2d70845f
BrunoAlan <alan.brunoab@gmail.com>
2025-01-25 17:53:36 -0300

    Refactor RootLayout to use QueryClientProvider without instantiating QueryClient

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,18 +1,16 @@
 import { Stack } from 'expo-router';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import * as SplashScreen from 'expo-splash-screen';
 import { ThemeProvider } from '../providers/Theme.provider';
 import LanguageProvider from '../providers/Language.provider';
+import QueryClientProvider from '../providers/QueryClient.provider';
 
 export default function RootLayout() {
-    const queryClient = new QueryClient();
-
     SplashScreen.preventAutoHideAsync();
     SplashScreen.setOptions({ duration: 1000, fade: true });
 
     return (
         <ThemeProvider>
-            <QueryClientProvider client={queryClient}>
+            <QueryClientProvider>
                 <LanguageProvider>
                     <Stack screenOptions={{ headerShown: false }}>
                         <Stack.Screen name='index' />
