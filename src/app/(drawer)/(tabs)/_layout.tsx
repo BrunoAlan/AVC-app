74c40a52c9b5e20b6cb5fb88c7fa77d80eabcf3f
BrunoAlan <alan.brunoab@gmail.com>
2025-01-25 11:17:27 -0300

    Add Search icon and integrate alerts in Home screen

--- src/app/(drawer)/(tabs)/_layout.tsx
+++ src/app/(drawer)/(tabs)/_layout.tsx
@@ -3,6 +3,7 @@ import { DrawerActions } from '@react-navigation/native';
 import { useNavigation } from 'expo-router';
 import { Tabs } from 'expo-router/tabs';
 import { Menu } from '@/lib/icons/menu';
+import { Search } from '@/lib/icons/search';
 
 export default function TabsLayout() {
     const navigation = useNavigation();
@@ -18,9 +19,19 @@ export default function TabsLayout() {
                         }}
                     />
                 ),
+                tabBarActiveTintColor: '#b4a258',
             }}
         >
-            <Tabs.Screen name='home' options={{ title: 'Home' }} />
+            <Tabs.Screen
+                name='home'
+                options={{
+                    title: 'Home',
+                    tabBarIcon: ({ focused }) => (
+                        <Search color={focused ? '#b4a258' : 'gray'} />
+                    ),
+                    tabBarActiveTintColor: '#b4a258',
+                }}
+            />
             <Tabs.Screen name='search' options={{ title: 'Search' }} />
             <Tabs.Screen name='bookings' options={{ title: 'Bookings' }} />
             <Tabs.Screen name='my-card' options={{ title: 'My card' }} />
--- src/app/(drawer)/(tabs)/home.tsx
+++ src/app/(drawer)/(tabs)/home.tsx
@@ -1,8 +1,27 @@
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
+import { AlertTriangle, Menu, Terminal } from 'lucide-react-native';
 import { View, Text } from 'react-native';
 const Home = () => {
     return (
         <View>
             <Text>Home screen</Text>
+            <Alert className='max-w-xl'>
+                <AlertTitle>Heads up!</AlertTitle>
+                <AlertDescription>
+                    You can use a terminal to run commands on your computer.
+                </AlertDescription>
+            </Alert>
+            <Alert
+                icon={AlertTriangle}
+                variant='destructive'
+                className='max-w-xl'
+            >
+                <AlertTitle>Danger!</AlertTitle>
+                <AlertDescription>
+                    High voltage. Do not touch. Risk of electric shock. Keep
+                    away from children.
+                </AlertDescription>
+            </Alert>
         </View>
     );
 };
