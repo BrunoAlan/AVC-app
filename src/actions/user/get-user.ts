ec96df7aca4ec65c7476c27fed52f98c657c755d
BrunoAlan <alan.brunoab@gmail.com>
2025-01-25 14:16:53 -0300

    Add type annotation for getUser function to improve type safety

--- src/actions/user/get-user.ts
+++ src/actions/user/get-user.ts
@@ -1,6 +1,7 @@
 import bookingFlowApi from '@/src/config/api/BookingFlowApi';
+import { type UserResponse } from '@/src/infrastructure/interfaces/user/user.response';
 
-export const getUser = async () => {
+export const getUser = async (): Promise<UserResponse | null> => {
     try {
         const response = await bookingFlowApi.get('/v2/users');
         return response.data;
