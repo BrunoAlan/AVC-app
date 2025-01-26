58b776838db2bd792f6e10fde27f68d45e6f50d5
BrunoAlan <alan.brunoab@gmail.com>
2025-01-26 01:30:26 -0300

    Add Typography constants for font sizes, weights, and families

---
+++ src/constants/theme/Typography.ts
@@ -0,0 +1,19 @@
+export const Typography = {
+    // Sizes
+    fontSizeXS: 12,
+    fontSizeSM: 14,
+    fontSizeMD: 16,
+    fontSizeLG: 20,
+    fontSizeXL: 24,
+    fontSizeXXL: 32,
+
+    // Weights
+    fontWeightLight: '300',
+    fontWeightRegular: '400',
+    fontWeightMedium: '500',
+    fontWeightBold: '700',
+
+    // fonts
+    fontFamilyRegular: 'MyCustomFont-Regular',
+    fontFamilyBold: 'MyCustomFont-Bold',
+};
