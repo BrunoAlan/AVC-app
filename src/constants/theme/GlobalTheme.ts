7bb3e04a2e1040b7cb0a37af1bac2573f3aa2eeb
BrunoAlan <alan.brunoab@gmail.com>
2025-01-26 01:30:38 -0300

    Add GlobalTheme constants to centralize theme management

---
+++ src/constants/theme/GlobalTheme.ts
@@ -0,0 +1,9 @@
+import { Colors } from './Colors';
+import { Spacing } from './Spacings';
+import { Typography } from './Typography';
+
+export const GlobalTheme = {
+    colors: { ...Colors },
+    typography: { ...Typography },
+    spacings: { ...Spacing },
+};
