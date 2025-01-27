d8dd833189c98dfddbcc37c8b93df41103f707f4
BrunoAlan <alan.brunoab@gmail.com>
2025-01-27 20:49:41 -0300

    Refactor contract store to use Zustand devtools and update selectedContract type

--- src/stores/contractStores.tsx
+++ src/stores/contractStores.tsx
@@ -1,11 +1,18 @@
-import { create } from 'zustand';
+import { create, type StateCreator } from 'zustand';
+import { devtools } from 'zustand/middleware';
 
 interface ContractStore {
-    selectedContract: string;
-    setSelectedContract: (contract: string) => void;
+    selectedContract: number | null;
+    setSelectedContract: (contract: number) => void;
 }
 
-export const useContractStore = create<ContractStore>((set) => ({
-    selectedContract: '',
+const storeAPI: StateCreator<ContractStore, [['zustand/devtools', never]]> = (
+    set
+) => ({
+    selectedContract: null,
     setSelectedContract: (contract) => set({ selectedContract: contract }),
-}));
+});
+
+export const useContractStore = create<ContractStore>()(
+    devtools(storeAPI, { name: 'contractStore' })
+);
