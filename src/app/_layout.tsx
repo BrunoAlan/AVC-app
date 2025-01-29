11dc61a902addee5dac50617acb618a7398c9489
BrunoAlan <alan.brunoab@gmail.com>
2025-01-29 14:45:42 -0300

    Add ErrorBoundary component to handle errors in the layout

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,9 +1,24 @@
-import { Stack } from 'expo-router';
+import { ErrorBoundaryProps, Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { ThemeProvider, useTheme } from '../providers/Theme.provider';
 import LanguageProvider from '../providers/Language.provider';
 import QueryClientProvider from '../providers/QueryClient.provider';
+import { View, Text } from 'react-native';
 
+export function ErrorBoundary({ error, retry }: ErrorBoundaryProps) {
+    return (
+        <View style={{ flex: 1, backgroundColor: 'red' }}>
+            <Stack.Screen
+                options={{
+                    headerShown: false,
+                    presentation: 'fullScreenModal',
+                }}
+            />
+            <Text>{error.message}</Text>
+            <Text onPress={retry}>Try Again?</Text>
+        </View>
+    );
+}
 export default function RootLayout() {
     SplashScreen.preventAutoHideAsync();
     SplashScreen.setOptions({ duration: 1000, fade: true });
