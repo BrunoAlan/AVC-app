8ea92c0821b73e56e89a67b7acdc615f17267d70
BrunoAlan <alan.brunoab@gmail.com>
2025-01-29 12:32:07 -0300

    Update getUser action to throw an error on failure instead of returning null

--- src/actions/user/get-user.ts
+++ src/actions/user/get-user.ts
@@ -1,12 +1,12 @@
 import bookingFlowApi from '@/src/config/api/BookingFlowApi';
 import { type UserResponse } from '@/src/infrastructure/interfaces/user/user.response';
 
-export const getUser = async (): Promise<UserResponse | null> => {
+export const getUser = async (): Promise<UserResponse> => {
     try {
         const response = await bookingFlowApi.get('/v2/users');
         return response.data;
     } catch (error) {
         console.error(error);
-        return null;
+        throw new Error('Error getting user');
     }
 };
