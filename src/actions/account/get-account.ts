54b2a994978a04aac8ea720a3266f6d962c1f600
BrunoAlan <alan.brunoab@gmail.com>
2025-01-29 12:31:36 -0300

    Add getAccount action to fetch user account data from API

---
+++ src/actions/account/get-account.ts
@@ -0,0 +1,12 @@
+import bookingFlowApi from '@src/config/api/BookingFlowApi';
+import { AccountResponse } from '@src/infrastructure/interfaces/account/account.response';
+
+export const getAccount = async (): Promise<AccountResponse> => {
+    try {
+        const { data } = await bookingFlowApi.get('/v2/users/account');
+        return data;
+    } catch (error) {
+        console.error(error);
+        throw new Error('Error getting account');
+    }
+};
