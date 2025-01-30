0a541e45a8299e372f6aeb8c7f22cbcfa449165d
BrunoAlan <alan.brunoab@gmail.com>
2025-01-30 11:52:17 -0300

    Add @shopify/flash-list package and clean up Home component

--- src/app/(drawer)/(tabs)/home.tsx
+++ src/app/(drawer)/(tabs)/home.tsx
@@ -1,32 +1,9 @@
-import { useContracts } from '@src/hooks/useContracts';
-import { useContractStore } from '@src/stores/contractStores';
-import { Link } from 'expo-router';
 import { View, Text, StyleSheet } from 'react-native';
-import { useEffect } from 'react';
-import Button from '@src/components/ui/Button';
-import { useAccount } from '@src/hooks/useAccount';
-import { FullScreenLoading } from '@src/components';
+
 const Home = () => {
-    const { setSelectedContract } = useContractStore();
-    const { isFetching: contractsFetching, contracts } = useContracts();
-    const { isFetching: accountFetching, account } = useAccount();
-
-    useEffect(() => {
-        if (contracts && !account?.hasMultipleContracts) {
-            setSelectedContract(contracts.at(0)?.id!);
-        }
-    }, [contracts]);
-
-    if (contractsFetching || accountFetching) {
-        return <FullScreenLoading />;
-    }
-
     return (
         <View style={styles.container}>
             <Text>home</Text>
-            <Link asChild href='/(modals)/contract'>
-                <Button title='Go to contract' onPress={() => {}} />
-            </Link>
         </View>
     );
 };
