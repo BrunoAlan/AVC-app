7f79c46a022df6abac57dc691199a6f871620d30
BrunoAlan <alan.brunoab@gmail.com>
2025-01-30 10:15:58 -0300

    Remove hardcoded blurhash from MyCard component and use account-specific image source

--- src/app/(drawer)/(tabs)/my-card.tsx
+++ src/app/(drawer)/(tabs)/my-card.tsx
@@ -2,16 +2,12 @@ import { StyleSheet, View } from 'react-native';
 import { Image } from 'expo-image';
 import { useAccount } from '@src/hooks/useAccount';
 const MyCard = () => {
-    const blurhash =
-        '|rF?hV%2WCj[ayj[a|j[az_NaeWBj@ayfRayfQfQM{M|azj[azf6fQfQfQIpWXofj[ayj[j[fQayWCoeoeaya}j[ayfQa{oLj?j[WVj[ayayj[fQoff7azayj[ayj[j[ayofayayayj[fQj[ayayj[ayfjj[j[ayjuayj[';
-
     const { account } = useAccount();
     return (
         <View style={styles.container}>
             <Image
                 style={styles.image}
                 source={`https://myclubprivileges.anantaravacationclub.com/api/images/cardface?email=${account?.primary.email}`}
-                placeholder={{ blurhash }}
                 contentFit='contain'
                 transition={1000}
             />
