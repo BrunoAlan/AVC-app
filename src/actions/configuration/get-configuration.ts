1723a80aab271f6fbfe919b29a029b6e7fdd3302
BrunoAlan <alan.brunoab@gmail.com>
2025-02-03 17:06:43 -0300

    Add getConfiguration function to fetch configuration data from BookingFlowApi

---
+++ src/actions/configuration/get-configuration.ts
@@ -0,0 +1,19 @@
+import bookingFlowApi from '@src/config/api/BookingFlowApi';
+import { Configuration } from '@src/infrastructure/interfaces/configuration/configuration.response';
+
+export const getConfiguration = async (): Promise<Configuration> => {
+    try {
+        const { data } = await bookingFlowApi.get<Configuration>(
+            '/ee/configurations',
+            {
+                params: {
+                    'system-id': 1,
+                },
+            }
+        );
+        return data;
+    } catch (error) {
+        console.log(error);
+        throw new Error('Error getting configuration');
+    }
+};
