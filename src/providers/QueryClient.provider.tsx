ad22a6dc23abc7894373ba71f5004916d3412af4
BrunoAlan <alan.brunoab@gmail.com>
2025-02-03 12:52:34 -0300

    Add React Query DevTools integration to QueryClientProvider

--- src/providers/QueryClient.provider.tsx
+++ src/providers/QueryClient.provider.tsx
@@ -3,10 +3,13 @@ import {
     QueryClientProvider as TanStackQueryClient,
 } from '@tanstack/react-query';
 import { PropsWithChildren } from 'react';
+import { useReactQueryDevTools } from '@dev-plugins/react-query';
 
 const queryClient = new QueryClient();
 
 const QueryClientProvider = ({ children }: PropsWithChildren) => {
+    useReactQueryDevTools(queryClient);
+
     return (
         <TanStackQueryClient client={queryClient}>
             {children}
