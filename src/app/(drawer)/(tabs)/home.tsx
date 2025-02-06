d9a2e898f3a71e9a6ffb26dbdbfb5b1da9a75848
BrunoAlan <alan.brunoab@gmail.com>
2025-02-06 09:32:39 -0300

    Replace button for setting dates with CustomCalendar component in Home screen

--- src/app/(drawer)/(tabs)/home.tsx
+++ src/app/(drawer)/(tabs)/home.tsx
@@ -1,17 +1,12 @@
 import { ThemedText } from '@src/components';
 import Button from '@src/components/ui/Button';
-import { useProperties } from '@src/hooks/useProperties';
 import { useSearchParamsStore } from '@src/stores/searchParamsStore';
 import { View, Text, StyleSheet } from 'react-native';
-import { useChannels } from '../../../hooks/useChannels';
-import { useConfiguration } from '@src/hooks/useConfiguration';
 import { useRegions } from '@src/hooks/useRegions';
 import CountryCitySearch from '@src/components/DestinationModal/DestinationModal';
+import CustomCalendar from '@src/components/Calendar/Calendar';
 
 const Home = () => {
-    const { checkIn, checkOut, setCheckIn, setCheckOut } =
-        useSearchParamsStore();
-
     const { regions } = useRegions({
         channelId: 'AVC_DEMO',
         language: 'en',
@@ -23,14 +18,7 @@ const Home = () => {
 
     return (
         <View style={styles.container}>
-            <Button
-                title='Set'
-                onPress={() => {
-                    setCheckIn('2025-03-01');
-                    setCheckOut('2025-04-02');
-                }}
-            ></Button>
-
+            <CustomCalendar />
             <CountryCitySearch data={regions} />
         </View>
     );
